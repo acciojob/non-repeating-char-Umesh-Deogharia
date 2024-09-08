2a763c53d6c70c07b3604bdf3e9113dd007d1b76
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-08 20:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 	let check = new Map();
-
 	for (let key of str) {
 		if (check.has(key)) {
-		check.key++;
+		check.set(key, check.get(key) + 1);
+	}else{
+		check.set(key , 1);
 	}
-		check.add(key);
-	
+	}
+
+	for (let key of str) {
+		if (check.get(key) === 1) {
+			return key;
+		}
 	}
 		return null;
 }
-// const input = prompt("Enter a string");
+const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
